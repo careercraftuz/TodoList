eb284e2812a5151b7ed0ad4bf587a1497dd42f30
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-03 16:13:00 +0500

    changed 'rest_framework' in installed apps

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # our apps
     'api',
-    'djangorestframework'
+
+    # rest framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
