093b15d5cad99e7419803cb60fd1d1cadb9f3a90
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-03 14:54:22 +0500

    added enpiont for get_task function

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import get_all_tasks
+from api.views import (
+    get_all_tasks,
+    get_task
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', get_all_tasks)
+    path('task/<int:id>', get_task)
 ]
