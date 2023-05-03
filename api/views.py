5b093e675060afa7d80a95f4f14498a801b9693f
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-03 10:07:48 +0500

    get all tasks

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Todo
+from .serializers import TaskSerializer, UserSerializer
+class TaskView(APIView):
+    '''get all tasks'''
+    def get(self, request: Request) -> Response:    
+        tasks = Todo.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        data = serializer.data
+        return Response(data)
+    
--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "api",
-    'django_rest_framework',
+    'rest_framework',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
