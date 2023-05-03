828acf7c18c9f374de7df37e58713059c6929b41
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-03 17:06:28 +0500

    Created URL path /tasks/id/update for update_task function view in api app

--- core/urls.py
+++ core/urls.py
@@ -5,11 +5,13 @@ from api.views import (
     get_all_tasks,
     get_task,
     delete_task,
+    update_task
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', get_all_tasks),
     path('tasks/<int:id>', get_task),
-    path('tasks/<int:id>/delete', delete_task)
+    path('tasks/<int:id>/delete', delete_task),
+    path('tasks/<int:id>/update', update_task)
 ]
