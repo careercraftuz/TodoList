5a92aab532e95b9c08a4c4ebaf5664764ecb16d9
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-03 08:54:18 +0500

    create todo models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    task=models.CharField(max_length=100)
+    description= models.TextField()
+    completed= models.BooleanField(default=False)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateField(auto_now=True)
+    def __str__(self) -> str:
+        return self.task
