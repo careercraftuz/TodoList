afba7abc67142a65534553d01992a263b8cf6cda
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-03 11:12:12 +0500

    create function for get all tasks

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Task
 
-# Create your views here.
+@api_view(["GET"])
+def get_all_tasks(request:Request):
+    if request.method == "GET":
+        tasks=Task.objects.all()
+        return Response({'tasks':tasks})
+    else:
+        return Response({'result':'Wrong method'})
