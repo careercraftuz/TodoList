12cf8f7597abe9780f25384b6c5976fe9a158f4b
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2023-05-03 17:35:18 +0500

    added api/ endpoint in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from api.views import (
-    get_all_tasks,
-    get_task,
-    delete_task,
-    update_task,
-    create_task
-)
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', get_all_tasks),
-    path('tasks/<int:id>', get_task),
-    path('tasks/<int:id>/delete', delete_task),
-    path('tasks/<int:id>/update', update_task),
-    path('create-task/',create_task),
+    path('api/', include('api.urls')),
 ]
