363dec07c0ec00b0463c74acca244aadecd91ed0
Doniyorbek <programmmerqety1@gmail.com>
2023-05-04 10:28:51 +0500

    description and status

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    status = models.BooleanField()
+    description = models.TextField(max_length=255, blank=True)
+    status = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
