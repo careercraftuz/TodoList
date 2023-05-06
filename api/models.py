ea7017bb4ef018c6e318e49de3247ba062e3f042
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-05-06 14:42:21 +0500

    added student field  to Task model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# import User model
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
@@ -6,6 +8,7 @@ class Task(models.Model):
     status = models.BooleanField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    student=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
