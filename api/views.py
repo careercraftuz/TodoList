b72cb375b9a612e4e076a502281246c47f910659
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-08 13:36:08 +0500

    added views for change create task as authentication

--- api/views.py
+++ api/views.py
@@ -6,7 +6,8 @@ from rest_framework.decorators import APIView
 from .models import Task
 from rest_framework.authtoken.models import Token
 from django.contrib import auth
-
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(["GET"])
 def get_all_tasks(request:Request):
@@ -28,53 +29,49 @@ def get_all_tasks(request:Request):
 
 
 class Get_task(APIView):   
-    def GET(request:Request, id:int):
+    permissions_classes =[IsAuthenticated]
+    def GET(self, request:Request, id:int):
             user= request.user
             try:
                 task=Task.objects.filter(user=user,id=id)
-                result={
-                    'id':task.id,
-                    'name':task.name,
-                    'description':task.description,
-                    'status':task.status,
-                    'created':task.created,
-                    'updated':task.updated
-                }
-                return Response(result)
+                serializer = TaskSerializer(task)
+                data=serializer.data
+                return Response(data)
             except:
                 return Response({'result':'Task not found'})
 
 
-@api_view(['POST'])
-def delete_task(request, id:int):
-    if request.method == 'POST':
+class DeleteTask:
+    def delete(request:Request, id:int):
+        user=request.user
         try:
-            task=Task.objects.get(id=id)
-            task.delete()
-            return Response({'result':'Task deleted'})
+                task=Task.objects.get(id=id)
+                task.delete()
+                return Response({'result':'Task deleted'})
         except:
-            return Response({'result':'Task not found'})
-        
+                return Response({'result':'Task not found'})
+            
 
 
-@api_view(['POST'])
-def create_task(request):
-    if request.method == 'POST':
+class CreateTask:
+    permissions_classes =[IsAuthenticated]
+    def post(self, request:Request)->Response:
+        user=request.user
+        task=request.data
         try:
-            task=request.data
-            object=Task.objects.create(
-                name=task['name'],
-                description=task['description'],
-                status=task['status']
-            )
-            object.save()
+            serialzer=TaskSerializer(user=user)
+            serialzer1=TaskSerializer(data=task)
+            if serialzer.is_valid():
+                Task.objects.filter(user=user)
+                serialzer1.save()
+                serialzer.save()     
             return Response({'result':'created'},status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({'result':f'bad request {e}'},status=status.HTTP_400_BAD_REQUEST)
-    return Response({'result':'Wrong method'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        
 
 @api_view(['POST'])
-def update_task(request,id:int):
+def update_task(request,id:int): 
     if request.method == 'POST':
         try:
             task = Task.objects.get(id=id)
