88f618fbb0057ace1b1c7dc5ce02801cc2c19412
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-08 13:46:07 +0500

    added url for change delete task as authentication

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     get_all_tasks,
     Get_task,
-    delete_task,
+    DeleteTask,
     update_task,
     CreateTask,
     LoginUser,
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path('tasks/', get_all_tasks),
     path('tasks/<int:id>',Get_task.as_view()),
-    path('tasks/<int:id>/delete', delete_task),
+    path('tasks/<int:id>/delete', DeleteTask.as_view()),
     path('tasks/<int:id>/update', update_task),
     path('createtask/',CreateTask.as_view()),
     path('new-token/',LoginUser.as_view()),
