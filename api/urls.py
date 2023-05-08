71cb59f32c223a9461a6a86e74fdb8583aae5931
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-05-08 14:13:58 +0500

    added views for change update task as authentication

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import (
     get_all_tasks,
     Get_task,
     DeleteTask,
-    update_task,
+    UpdateTask,
     CreateTask,
     LoginUser,
     Logoutuser
@@ -13,8 +13,8 @@ from .views import (
 urlpatterns = [
     path('tasks/', get_all_tasks),
     path('tasks/<int:id>',Get_task.as_view()),
-    path('tasks/<int:id>/delete', DeleteTask.as_view()),
-    path('tasks/<int:id>/update', update_task),
+    path('tasks/delete/<int:id>/', DeleteTask.as_view()),
+    path('tasks/update/<int:id>/', UpdateTask.as_view()),
     path('createtask/',CreateTask.as_view()),
     path('new-token/',LoginUser.as_view()),
     path('logout/',Logoutuser.as_view()),
